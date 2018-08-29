9298501f0c1fc2b1be5a0e447d21fda6696dfea2
samuel <samuel@vastdata.com>
2018-08-29 20:01:40 +0300

    s3_stress: adding ability to send s3 object metadata on PUT request

--- s3_stress/s3_stress_runner.py
+++ s3_stress/s3_stress_runner.py
@@ -3,7 +3,6 @@
 """
 S3 Stress testing tool - 2018 (c)
 """
-
 __author__ = 'samuel'
 
 import gevent.monkey
@@ -15,6 +14,7 @@ import argparse
 import random
 import requests
 import os
+import uuid
 from concurrent.futures import ProcessPoolExecutor
 from string import ascii_letters
 from threading import Event
@@ -28,14 +28,23 @@ from s3_stress.utils import config
 logger = server_logger.Logger(name=__name__).logger
 stop_event = Event()
 
+KB1 = 1024
 MAX_WORKER_THREADS = 50
 MAX_WORKER_PROCESSES = 20
 RANDOM_CHUNK_2M = os.urandom(2 * 1024 * 1024)
 RANDOM_CHUNK_1M = os.urandom(1 * 1024 * 1024)
 RANDOM_CHUNK_256K = os.urandom(256 * 1024)
 RANDOM_CHUNK_4K = os.urandom(4 * 1024)
-DATA_CHUNKS = {"4K": 4 * 1024, "256K": 256 * 1024, "1M": 1024 * 1024, "2M": 2 * 1024 * 1024}
+DATA_CHUNKS = {"4K": 4 * 1024, "256K": 256 * 1024, "1M": 1024 * 1024, "2M": 2 * 1024 * 1024, "5M": 5 * 1024 * 1024,
+               "10M": 10 * 1024 * 1024, "20M": 20 * 1024 * 1024}
+DATA_BUFFERS = {"4K": os.urandom(4 * 1024), "256K": os.urandom(256 * 1024), "1M": os.urandom(1024 * 1024),
+                "2M": os.urandom(2 * 1024 * 1024), "5M": os.urandom(5 * 1024 * 1024),
+                "10M": os.urandom(10 * 1024 * 1024), "20M": os.urandom(20 * 1024 * 1024)}
 TMP_FILE = 'tmpfile'
+
+
+def get_random_string(length):
+    return ''.join(random.choice(ascii_letters) for _ in range(length))
 
 
 def generate_valid_object_name_ascii(length):
@@ -47,6 +56,19 @@ def generate_valid_object_name():
     return "{}".format(random.randint(1, 64000))
 
 
+def generate_metadata_dict(md_dict_size=KB1 * 2):
+    size = 0
+    md_dict = {}
+    while size < md_dict_size:
+        md_key = uuid.uuid4().hex[:4]
+        #  We'll use weighted probability to generate various metadata sizes until 2KB is filled up
+        md_value = get_random_string(random.choices([1, 4, 8, 16, 32, 64, 128, 256, 512, 1024],
+                                                    weights=[20, 20, 20, 20, 10, 6, 1, 1, 1, 1])[0])
+        md_dict[f"x-amz-meta-{md_key}"] = md_value
+        size += len(md_key + md_value)
+    return md_dict
+
+
 def get_args():
     parser = argparse.ArgumentParser(
         description='S3 Stress tool (c) - 2018')
@@ -55,21 +77,23 @@ def get_args():
     parser.add_argument('-t', '--threads', type=int, help="Number of concurrent threads", default=MAX_WORKER_THREADS)
     parser.add_argument('-p', '--processes', type=int, help="Number of concurrent processes",
                         default=MAX_WORKER_PROCESSES)
-    parser.add_argument('-s', '--size', choices=['4K', '256K', '1M', '2M'], default="4K", help="Data file size")
+    parser.add_argument('-s', '--size', choices=['4K', '256K', '1M', '2M', '5M', '10M', '20M', 'MIXED'], default="4K",
+                        help="Data file size")
     parser.add_argument('-w', '--workload', choices=['GET', 'PUT', 'DELETE', 'MIXED'], default="PUT",
                         help="Workload")
     parser.add_argument('-m', '--multibucket', action="store_true", help="Creates new bucket for each process")
+    parser.add_argument('--metadata', action="store_true", help="Generated MetaData attributes for each object")
     args = parser.parse_args()
     return args
 
 
-def gevent_pool_starter(method, bucket_name, path, threads, data_chunk_size, s3_config):
+def gevent_pool_starter(method, bucket_name, path, threads, metadata, data_chunk_size, s3_config):
     objects_queue = Queue()
     pool = Pool(threads)
     for i in range(threads):
         pool.apply_async(handle_http_methods(method if method != 'MIXED' else random.choice(['GET', 'PUT', 'DELETE'])),
                          kwds=dict(bucket_name=bucket_name, path=path, objects_queue=objects_queue,
-                                   thread_id="{}".format(10000 + i), data_chunk_size=data_chunk_size,
+                                   thread_id="{}".format(10000 + i), data_chunk_size=data_chunk_size, metadata=metadata,
                                    config=s3_config))
     pool.join()
 
@@ -86,7 +110,9 @@ def handle_http_methods(method):
 def s3_put_worker(**kwargs):
     bucket_name = kwargs['bucket_name']
     path = kwargs['path']
-    data_chunk = os.urandom(kwargs['data_chunk_size'])
+    metadata = generate_metadata_dict() if kwargs['metadata'] else {}
+    data_chunk = random.choice(list(DATA_BUFFERS.values())) if kwargs['data_chunk_size'] == 'MIXED' \
+        else DATA_BUFFERS[kwargs['data_chunk_size']]
     logger.info("PUT Worker started: {}".format(kwargs))
     path = os.path.join(path, kwargs['thread_id'])
     path_counter = 100
@@ -98,7 +124,7 @@ def s3_put_worker(**kwargs):
             full_object_name = "/".join([path, str(path_counter), object_name])
             try:
                 url = "/".join([kwargs['endpoint_url'], bucket_name, full_object_name])
-                res = session.put(url, data=data_chunk, auth=kwargs['auth'])
+                res = session.put(url, data=data_chunk, auth=kwargs['auth'], headers=metadata)
                 res.raise_for_status()
             except (requests.ConnectionError, requests.HTTPError) as requests_err:
                 logger.error("{} : PUT {}".format(requests_err.args[0], full_object_name))
@@ -170,27 +196,28 @@ def s3_delete_worker(**kwargs):
 def mkbucket(**kwargs):
     s3 = kwargs['s3_resource']
     bucket_name = kwargs['bucket_name']
-    bucket = s3.create_bucket(Bucket=bucket_name)
-    logger.debug("Bucket {} created".format(bucket.name))
+    try:
+        bucket = s3.create_bucket(Bucket=bucket_name)
+        logger.debug("Bucket {} created".format(bucket.name))
+    except ClientError as boto_err:
+        # allow rewriting existing bucket content
+        if boto_err.response['ResponseMetadata']['HTTPStatusCode'] != 409 and not kwargs['ignore_existing']:
+            raise boto_err
 
 
 def process_pool_starter(args, data_chunk_size, s3_config):
-    try:
-        if not args.multibucket:
-            mkbucket(config=s3_config, bucket_name=args.bucket)
-        else:
-            for i in range(args.processes):
-                mkbucket(config=s3_config, bucket_name=f"{args.bucket}-{i}")
-    except ClientError as boto_err:
-        if boto_err.response['ResponseMetadata']['HTTPStatusCode'] != 409:  # allow rewriting existing bucket content
-            raise boto_err
+    if not args.multibucket:
+        mkbucket(config=s3_config, bucket_name=args.bucket, ignore_existing=True)
+    else:
+        for i in range(args.processes):
+            mkbucket(config=s3_config, bucket_name=f"{args.bucket}-{i}", ignore_existing=True)
 
     with ProcessPoolExecutor(args.processes) as s3_process_pool:
         for i in range(args.processes):
             path = '/' + (args.hostname + '/' if args.hostname is not None else "") + str(i)
             logger.info("Started work on path {}".format(path))
             bucket_name = args.bucket if not args.multibucket else f"{args.bucket}-{i}"
-            s3_process_pool.submit(gevent_pool_starter, args.workload, bucket_name, path, args.threads,
+            s3_process_pool.submit(gevent_pool_starter, args.workload, bucket_name, path, args.threads, args.metadata,
                                    data_chunk_size, s3_config)
 
 
@@ -199,7 +226,7 @@ def main():
     s3_config = config.ensure_config()
     if not s3_config:
         raise RuntimeError("Bad or missing config file")
-    process_pool_starter(args, DATA_CHUNKS[args.size], s3_config)
+    process_pool_starter(args, args.size, s3_config)
 
 
 if __name__ == "__main__":
