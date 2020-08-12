763e3a38208e70f8335cdf4698ae809fa90b7e3a
samuel <samuel@vastdata.com>
2020-08-12 04:55:27 +0300

    fix release version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="s3_stress",
-    version="1.0.0",
+    version="0.9.1",
     author="Samuel Shapiro",
     author_email="samuel@vast.com",
     description="S3 load & stress tool",
